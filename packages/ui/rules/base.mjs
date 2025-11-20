5bd0d9f502fbc41e7d2e1f0d6efa1ecc50b5b751
cosmicintruder <littlegreenbagg@googlemail.com>
2025-11-20 12:47:05 +0400

    base rules changes test

--- packages/ui/rules/base.mjs
+++ packages/ui/rules/base.mjs
@@ -27,7 +27,6 @@ export default {
   "no-console": "off",
   "default-param-last": "off",
   "arrow-body-style": ["warn", "as-needed"],
-  "no-unused-vars": "off",
   "@typescript-eslint/ban-ts-comment": "off",
   "@typescript-eslint/no-empty-function": "off",
   "@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
